5e02149b9a1918657d7cf37c0395c0cede0a2a02
Al-un <alun.sng@gmail.com>
2020-07-11 21:01:19 +0900

    vue nuxtjs: update Vuetify configuration to use vue-i18n messages

--- vue_nuxtjs-full-setup/nuxt.config.ts
+++ vue_nuxtjs-full-setup/nuxt.config.ts
@@ -1,4 +1,4 @@
-import colors from 'vuetify/es5/util/colors'
+// import colors from 'vuetify/es5/util/colors'
 
 // import i18nMsgs from './i18n/'
 
@@ -73,21 +73,22 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
+    optionsPath: 'vuetify.options.js',
+    // customVariables: ['~/assets/variables.scss'],
+    // theme: {
+    //   dark: true,
+    //   themes: {
+    //     dark: {
+    //       primary: colors.blue.darken2,
+    //       accent: colors.grey.darken3,
+    //       secondary: colors.amber.darken3,
+    //       info: colors.teal.lighten1,
+    //       warning: colors.amber.base,
+    //       error: colors.deepOrange.accent4,
+    //       success: colors.green.accent3,
+    //     },
+    //   },
+    // },
   },
   /*
    ** Build configuration
@@ -95,7 +96,7 @@ export default {
    */
   build: {},
   i18n: {
-    defaultLocale: 'fr',
+    defaultLocale: 'en',
     // --- If lazy=true is required, langDir and locales-as-array would be
     // --- already ready
     // https://github.com/nuxt-community/nuxt-i18n/blob/145f3b2a080a91028fd5ef59f9c8bd88755d3b4b/docs/lazy-load-translations.md

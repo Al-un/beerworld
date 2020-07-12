2d96cd5bf6207a8648837d09812fa27f561cd4e2
Al-un <alun.sng@gmail.com>
2020-07-12 19:08:00 +0900

    vue nuxtjs: minor update in BeerTable and beers list update

--- vue_nuxtjs-full-setup/components/BeerTable.vue
+++ vue_nuxtjs-full-setup/components/BeerTable.vue
@@ -5,12 +5,15 @@
       <v-icon>mdi-home</v-icon>
     </div>
 
-    <v-data-table :headers="headers" :items="beers" item-key="id"></v-data-table>
+    <v-data-table
+      :headers="headers"
+      :items="beers"
+      :items-per-page="5"
+    ></v-data-table>
 
     <v-chip close color="teal" text-color="white" close-icon="mdi-delete">
-      <v-avatar left>
-        <v-icon>mdi-checkbox-marked-circle</v-icon>
-      </v-avatar>Confirmed
+      <v-avatar left> <v-icon>mdi-checkbox-marked-circle</v-icon> </v-avatar
+      >Confirmed
     </v-chip>
   </div>
 </template>
@@ -18,7 +21,7 @@
 <script lang="ts">
 import Vue from 'vue'
 
-import { Beer } from '@/models'
+import { Beer } from '~/models'
 
 interface Props {
   beers: Beer[]
@@ -34,12 +37,55 @@ interface Computed {
 }
 
 const beersList: Beer[] = [
-  { id: 1, name: 'Grimbergen', country: 'Belgium', type: 'Abbey beer' },
-  { id: 2, name: 'Grimbergen 2', country: 'Belgium', type: 'Abbey beer' },
-  { id: 3, name: 'Grimbergen 3', country: 'Belgium', type: 'Abbey beer' },
-  { id: 4, name: 'Affligem', country: 'Belgium', type: 'Abbey beer' },
-  { id: 5, name: 'Affligem 2', country: 'Belgium', type: 'Abbey beer' },
-  { id: 6, name: 'Affligem 3', country: 'Belgium', type: 'Abbey beer' },
+  {
+    id: 1,
+    name: 'Grimbergen',
+    country: 'Belgium',
+    type: 'Blonde Ale - Belgian',
+  },
+  {
+    id: 2,
+    name: 'Grimbergen Double-Ambree',
+    country: 'Belgium',
+    type: 'Dubbel',
+  },
+  {
+    id: 3,
+    name: 'Leffe Blonde',
+    country: 'Belgium',
+    type: 'Blonde Ale - Belgian',
+  },
+  {
+    id: 4,
+    name: 'Leffe Radieuse',
+    country: 'Belgium',
+    type: 'Strong Ale - Belgian Dark',
+  },
+  {
+    id: 5,
+    name: 'La Chouffe',
+    country: 'Belgium',
+    type: 'Strong Ale - Belgian Pale',
+  },
+  { id: 6, name: 'Tripel Karmeliet', country: 'Belgium', type: 'Tripel' },
+  {
+    id: 7,
+    name: 'Hoegaarden',
+    country: 'Belgium',
+    type: 'Wheat Beer - Witbier',
+  },
+  {
+    id: 8,
+    name: 'Paulaner Original Münchner Dunkel ',
+    country: 'Germany',
+    type: 'Lager - Munich Dunkel',
+  },
+  {
+    id: 9,
+    name: 'Affligem Blond',
+    country: 'Belgium',
+    type: 'Blonde Ale - Belgian',
+  },
 ]
 
 export default Vue.extend<{}, {}, Computed, Props>({
@@ -51,6 +97,10 @@ export default Vue.extend<{}, {}, Computed, Props>({
   computed: {
     headers() {
       return [
+        // { text: 'beer.id', value: 'id' },
+        // { text: 'beer.name', value: 'name' },
+        // { text: 'beer.country', value: 'country' },
+        // { text: 'beer.type', value: 'type' },
         { text: this.$i18n.t('beer.id') as string, value: 'id' },
         { text: this.$i18n.t('beer.name') as string, value: 'name' },
         { text: this.$i18n.t('beer.country') as string, value: 'country' },

6771ba82b35648e6158f636cfbf7d346c118c522
Al-un <alun.sng@gmail.com>
2020-09-05 00:05:02 +0900

    nodejs web+orm: add try/catch to express handlers

--- nodejs/handlers/express.ts
+++ nodejs/handlers/express.ts
@@ -18,45 +18,70 @@ export class ExpressHandler implements Handler {
     req: express.Request<{}, Beer, Beer>,
     res: express.Response
   ) => {
-    let newBeer = req.body;
-    newBeer = await this._dao.createBeer(newBeer);
+    try {
+      let newBeer = req.body;
+      newBeer = await this._dao.createBeer(newBeer);
 
-    res.json(newBeer);
+      res.json(newBeer);
+    } catch (error) {
+      res.status(500);
+      res.json({ error });
+    }
   };
 
   deleteBeer = async (req: express.Request, res: express.Response) => {
-    const beerId = req.params["beerId"];
-    const deleteCount = await this._dao.deleteBeer(beerId);
+    try {
+      const beerId = req.params["beerId"];
+      const deleteCount = await this._dao.deleteBeer(beerId);
 
-    res.json({ deleteCount });
+      res.json({ deleteCount });
+    } catch (error) {
+      res.status(500);
+      res.json({ error });
+    }
   };
 
   getBeer = async (req: express.Request, res: express.Response) => {
-    const beerId = req.params["beerId"];
-    const beer = await this._dao.getBeer(beerId);
+    try {
+      const beerId = req.params["beerId"];
+      const beer = await this._dao.getBeer(beerId);
 
-    if (beer) {
-      res.json(beer);
-    } else {
-      res.sendStatus(404);
+      if (beer) {
+        res.json(beer);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      res.status(500);
+      res.json({ error });
     }
   };
 
   listBeers = async (req: express.Request, res: express.Response) => {
-    const beers = await this._dao.listBeers();
+    try {
+      const beers = await this._dao.listBeers();
 
-    res.json(beers);
+      res.json(beers);
+    } catch (error) {
+      res.status(500);
+      res.json({ error });
+    }
   };
 
   updateBeer = async (req: express.Request, res: express.Response) => {
-    const beerId = req.params["beerId"];
-    let beer = req.body;
-    beer = await this._dao.updateBeer(beer, beerId);
+    try {
+      const beerId = req.params["beerId"];
+      let beer = req.body;
+      beer = await this._dao.updateBeer(beer, beerId);
 
-    if (beer) {
-      res.json(beer);
-    } else {
-      res.sendStatus(404);
+      if (beer) {
+        res.json(beer);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      res.status(500);
+      res.json({ error });
     }
   };
 }

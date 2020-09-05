d125151e7f992f232c7e198ce6e265c2fc12d700
Al-un <alun.sng@gmail.com>
2020-09-05 13:15:27 +0900

    nodejs web+orm: use a JS file for Sequelize database access parameters

---
+++ nodejs/dao/sequelize/config/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "padawan",
+    password: "padawan",
+    database: "bw_nodejs",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};

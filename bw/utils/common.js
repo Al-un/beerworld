c7441b3e885ee916298cd6261e22b19981be3b42
Al-un <alun.sng@gmail.com>
2021-01-03 19:49:16 +0900

    bw: basic utilities

---
+++ bw/utils/common.js
@@ -0,0 +1,6 @@
+export const APP_WRAPPER = document.querySelector("#bw-app-wrapper");
+export const APP_NAV_BACKDROP = document.querySelector("#bw-nav-backdrop");
+export const APP_NAV_DRAWER = document.querySelector("#bw-nav-drawer");
+export const APP_HEADER = document.querySelector("#bw-app-header");
+export const APP_CONTENT = document.querySelector("#bw-app-content");
+export const APP_FOOTER = document.querySelector("#bw-app-footer");

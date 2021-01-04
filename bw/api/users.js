7c237d89728408ea6fc780c85298529829bebf52
Al-un <alun.sng@gmail.com>
2021-01-05 00:32:53 +0900

    bw: update login API call

--- bw/api/users.js
+++ bw/api/users.js
@@ -5,11 +5,21 @@ const mockUser = {
   name: "Plop",
 };
 
+/**
+ * Converted from mockUser on https://jwt.io/
+ */
+const mockToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTYiLCJuYW1lIjoiUGxvcCIsImlhdCI6MTUxNjIzOTAyMn0.7UAcUx1XZG_dkJIvfZEqCslRhw_Y_9y_Orf8NL5C-p4";
+
 const mockUsersApi = {
-  login: async ({ email, password }) => {
-    if (email === "plop@plop.com" && password === "plop") {
+  login: async ({ login, password }) => {
+    if (login === "plop@plop.com" && password === "plop") {
       return new Promise((resolve) => {
-        setTimeout(() => resolve(mockUser), generateDelay());
+        setTimeout(() => resolve({ accessToken: mockToken }), generateDelay());
+      });
+    } else {
+      return new Promise((_, reject) => {
+        setTimeout(() => reject(), generateDelay());
       });
     }
   },
@@ -20,7 +30,7 @@ const mockUsersApi = {
 };
 
 const realUsersApi = {
-  login: async ({ email, password }) => {
+  login: async ({ login, password }) => {
     throw new Error("Not implemented");
   },
 

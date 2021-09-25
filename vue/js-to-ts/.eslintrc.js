7f0d4a42decfe2c812536e94b57e6831675ab556
Al-un <alun.sng@gmail.com>
2021-09-25 16:42:45 +0900

    vue js-to-ts: add typescript

--- vue/js-to-ts/.eslintrc.js
+++ vue/js-to-ts/.eslintrc.js
@@ -3,17 +3,66 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+
+  // ----------
+  // Uncomment the TypeScript specific presets to enable TypeScript linting
+  // rules. Do not rearrange presets order as order matters. The plugins list
+  // is copied from a Vue 2 project freshly generated with @vue/cli
+  //
+  // If TypeScript presets are used, reducing @typescript-eslint/ban-ts-comment
+  // down to "warn" instead of "error" might be required as typing the whole
+  // project cannot be done quickly.
+  // ----------
+  extends: [
+    // Available linting presets, from loosest to strictest:
+    //
+    // plugin:vue/base
+    // plugin:vue/vue3-essential
+    // plugin:vue/vue3-strongly-recommended
+    // plugin:vue/vue3-recommended
+    // plugin:vue/essential
+    // plugin:vue/strongly-recommended
+    // plugin:vue/recommended
+    //
+    // From: https://eslint.vuejs.org/rules/
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    // '@vue/typescript/recommended', // --> see TS override below
+    "@vue/prettier",
+    // '@vue/prettier/@typescript-eslint',
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    ecmaVersion: 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // v-html is currently being used so limit it to warning only
+    // https://eslint.vuejs.org/rules/no-v-html.html
+    "vue/no-v-html": "warn",
+    // To enforce consistency between component name and file name
+    // https://eslint.vuejs.org/rules/match-component-file-name.html
+    "vue/match-component-file-name": ["warn", { extensions: ["vue"] }],
   },
   overrides: [
+    // TypeScript files. As the project is currently hybrid using both JavaScript
+    // and TypeScript components, linting rules must be adapted for each component
     {
-      files: ["**/*.spec.{j,t}s?(x)"],
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:vue/essential",
+        "eslint:recommended",
+        "@vue/typescript/recommended",
+        "@vue/prettier",
+        "@vue/prettier/@typescript-eslint",
+      ],
+      rules: {
+        "@typescript-eslint/ban-ts-comment": "off",
+      },
+    },
+    // Jest / Testing files
+    {
+      files: ["**/*.spec.{j,t}s?(x)", "jest.setup.js", "tests/**.*{j,t}s"],
       env: {
         jest: true,
       },
--- vue/js-to-ts/src/main.ts
+++ vue/js-to-ts/src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// @ts-ignore
 Vue.config.productionTip = false;
 
 new Vue({
